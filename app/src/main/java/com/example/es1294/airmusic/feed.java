9b839bef19ecfe7484b0b4a28f62be9e455acee5
mahiaash <mahiaash@buffalo.edu>
2018-09-23 22:41:58 -0400

    This is the feed page. It has images of the friends that the user has , their names, and the song and artist. Once clicked a text box should appear saying "You clicked on <insert friend name here>" That is how you know it works. There is a scrolling animation so it loads the list better.

--- app/src/main/java/com/example/es1294/airmusic/feed.java
+++ app/src/main/java/com/example/es1294/airmusic/feed.java
@@ -1,12 +1,19 @@
 package com.example.es1294.airmusic;
 
 import android.content.Intent;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class feed extends AppCompatActivity {
 
@@ -14,8 +21,44 @@ public class feed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        ListView list = findViewById(R.id.feed_view);
+        final ArrayList<Friends> array = new ArrayList<>();
+        Friends a = new Friends("Cloud", "Radioactive", "Imagine Dragons", "drawable://" + R.drawable.cloud);
+        Friends b = new Friends("Dovahkin", "Sovngarde", "Jermey Soule", "drawable://" + R.drawable.riften);
+        Friends c = new Friends("Lara Croft", "Cherry Bomb", "The Runaways", "drawable://" + R.drawable.shadow);
+        Friends d = new Friends("Thor ", "Immigrant Song", "Led Zeppelin", "drawable://" + R.drawable.godofhammers);
+        Friends e = new Friends("Iron man", "Highway to Hell", "AC/DC", "drawable://" + R.drawable.ironman);
+        Friends f = new Friends("Loki ", "Bitch Better Have My Money", "Rhianna", "drawable://" + R.drawable.lokiliesmith);
+        Friends g = new Friends("Daenerys", "Queen", "Janelle Monae", "drawable://" + R.drawable.motherofdragons);
+        Friends h = new Friends("Avatar Korra", "Turn It Down For What", "DJ Snake, Lil Jon", "drawable://" + R.drawable.avatarkorra);
+        Friends i = new Friends("Hella", "Chun - Li", "Nicki Minaj", "drawable://" + R.drawable.hella);
+        Friends j = new Friends("Terry Mcginnis", "Batman Beyond Theme", "Kristopher Carter", "drawable://" + R.drawable.terry);
+        Friends k = new Friends("Defenders", "We Will Rock You", "Queen", "drawable://" + R.drawable.defenders);
 
+        array.add(a);
+        array.add(b);
+        array.add(c);
+        array.add(d);
+        array.add(e);
+        array.add(f);
+        array.add(g);
+        array.add(h);
+        array.add(i);
+        array.add(j);
+        array.add(k);
+
+
+        Feed_List_Adapter adapter = new Feed_List_Adapter(this, R.layout.adapter_view_layout, array);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("feed","onItemClick: array " + array.get(i).getName());
+                Toast.makeText(feed.this, "You clicked on: " + array.get(i).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
