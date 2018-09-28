46637dbaf5bf08c74fc2752b8d383b6e1652044b
es1294 <johnbola@buffalo.edu>
2018-09-28 14:37:26 -0400

    When you pull you should be able to see all the added png files. -project should now be up to date

--- app/src/main/java/com/example/es1294/airmusic/MainActivity.java
+++ app/src/main/java/com/example/es1294/airmusic/MainActivity.java
@@ -16,22 +16,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button play_button;
-    SeekBar song_progress;
-    SeekBar volume_control;
-    TextView elapsed_time;
-    TextView remaining_time;
-    MediaPlayer mediaPlayer;
-    int total_time;
+    Button play_button;     // button on music screen to play the current song
+    SeekBar song_progress;  //seekbar that allows user to fast foward or rewind
+    SeekBar volume_control; //seekbar that allows user to set the volume
+    TextView elapsed_time;  //text that shows the current time on the song
+    TextView remaining_time;    //text that shows the remaining time on the song
+    MediaPlayer mediaPlayer;    //object that plays the music
+    int total_time; //sets the max of the song so we don't scroll pass the actual playing time of the song
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        //instantiating the variables to the contents in the drawable files
+
         play_button = (Button)findViewById(R.id.play_button);
         elapsed_time = (TextView) findViewById(R.id.elapsed_time);
         remaining_time = (TextView) findViewById(R.id.remaining_time);
+
+        //creates the mediaPlayer object to play the current song
+        //sets the volume to half
+        //and gets the duration of the song
 
         mediaPlayer = mediaPlayer.create(this, R.raw.blue_dream);
         mediaPlayer.setLooping(true);
@@ -39,10 +45,29 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.setVolume(0.5f , 0.5f);
         total_time = mediaPlayer.getDuration();
 
+        //instantiates the variable song_progress to the seekbar in the MainActivity.xml
+
         song_progress = (SeekBar) findViewById(R.id.song_progress);
         song_progress.setMax(total_time);
 
         volume_control = (SeekBar) findViewById(R.id.volume_control);
+        volume_control.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                float volumeStatus = progress /100f;
+                mediaPlayer.setVolume(progress, progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void playButtonClick(View view){
