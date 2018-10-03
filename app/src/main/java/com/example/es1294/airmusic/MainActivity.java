49a892f17ddde6b9bd2efaea3eb1b8149acabead
sohamdesh <sohamdes@buffalo.edu>
2018-10-03 13:15:50 -0400

    Login page

--- app/src/main/java/com/example/es1294/airmusic/MainActivity.java
+++ app/src/main/java/com/example/es1294/airmusic/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
