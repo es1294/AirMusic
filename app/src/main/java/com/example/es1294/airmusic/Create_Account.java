99f070fdeb91de08f449a4d031fe79d8981215f2
annaspri <annaspri@buffalo.edu>
2018-10-20 10:26:34 -0400

    Closed task #33

--- app/src/main/java/com/example/es1294/airmusic/Create_Account.java
+++ app/src/main/java/com/example/es1294/airmusic/Create_Account.java
@@ -7,6 +7,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Create_Account extends Activity {
+
+    DatabaseHelper helper = new DatabaseHelper(this);
+
 @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,28 +18,44 @@ public class Create_Account extends Activity {
 
 
     public void onButtonClick(View v){
+        //Toast genMessage = Toast.makeText(Create_Account.this, "Button Pushed?", Toast.LENGTH_SHORT);
+        //genMessage.show();
         if(v.getId() == R.id.signin){
+            Toast onClickMessage = Toast.makeText(Create_Account.this, "Button Pushed yes", Toast.LENGTH_SHORT);
+            onClickMessage.show();
             EditText username = (EditText) findViewById(R.id.username);
             EditText password = (EditText) findViewById(R.id.password);
             EditText confrimpass = (EditText) findViewById(R.id.confirmpassword);
             EditText email = (EditText) findViewById(R.id.email);
-            EditText confrimem = (EditText) findViewById(R.id.confirmpassword);
+            EditText confirmem = (EditText) findViewById(R.id.confirmemail);
 
             String userstr = username.getText().toString();
             String passstr = password.getText().toString();
             String confrimpassstr = confrimpass.getText().toString();
             String emailstr = email.getText().toString();
-            String confrimemstr = confrimem.getText().toString();
+            String confirmemstr = confirmem.getText().toString();
 
             if(!passstr.equals(confrimpassstr)){
                 // error message
                 Toast pass = Toast.makeText(Create_Account.this, "Passwords do not match",Toast.LENGTH_SHORT);
                 pass.show();
             }
-            if(!emailstr.equals(confrimemstr)){
+            if(!emailstr.equals(confirmemstr)){
                 // error message
                 Toast mail = Toast.makeText(Create_Account.this, "Email does not match", Toast.LENGTH_SHORT);
                 mail.show();
+            }
+            else{
+                //add to database
+                Toast message = Toast.makeText(Create_Account.this, "Reached add to database", Toast.LENGTH_SHORT);
+                message.show();
+                User user = new User();
+                user.setUsername(userstr);
+                user.setPassword(passstr);
+                user.setEmail(emailstr);
+
+                helper.insertUser(user);
+
             }
         }
     }
--- app/src/main/java/com/example/es1294/airmusic/User.java
+++ app/src/main/java/com/example/es1294/airmusic/User.java
@@ -16,28 +16,28 @@ public class User {
     public void setName(String name){
         this.name = name;
     }
-    public String getName(String name){
+    public String getName(){
         return this.name;
     }
     //email
     public void setEmail(String email){
         this.email = email;
     }
-    public String getEmail(String email){
+    public String getEmail(){
         return this.email;
     }
     //username
     public void setUsername(String username){
         this.username = username;
     }
-    public String getUsername(String username){
+    public String getUsername(){
         return this.username;
     }
     //password
     public void setPassword(String password){
         this.password = password;
     }
-    public String getPassword(String password){
+    public String getPassword(){
         return this.password;
     }
 //end getters and setters
