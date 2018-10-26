bf923cdf07668ca92d99105c93c6f41803335e52
annaspri <annaspri@buffalo.edu>
2018-10-26 18:57:04 -0400

    Closed tasks #29 and #32, UI improvements

--- app/src/main/java/com/example/es1294/airmusic/Create_Account.java
+++ app/src/main/java/com/example/es1294/airmusic/Create_Account.java
@@ -2,6 +2,7 @@ package com.example.es1294.airmusic;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 public class Create_Account extends Activity {
 
     DatabaseHelper helper = new DatabaseHelper(this);
+    boolean passFlag = true;
+    boolean emailFlag = true;
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +25,8 @@ public class Create_Account extends Activity {
         //Toast genMessage = Toast.makeText(Create_Account.this, "Button Pushed?", Toast.LENGTH_SHORT);
         //genMessage.show();
         if(v.getId() == R.id.signin){
-            Toast onClickMessage = Toast.makeText(Create_Account.this, "Button Pushed yes", Toast.LENGTH_SHORT);
-            onClickMessage.show();
+            //Toast onClickMessage = Toast.makeText(Create_Account.this, "Button Pushed yes", Toast.LENGTH_SHORT);
+            //onClickMessage.show();
             EditText username = (EditText) findViewById(R.id.username);
             EditText password = (EditText) findViewById(R.id.password);
             EditText confrimpass = (EditText) findViewById(R.id.confirmpassword);
@@ -36,19 +39,33 @@ public class Create_Account extends Activity {
             String emailstr = email.getText().toString();
             String confirmemstr = confirmem.getText().toString();
 
+            if(userstr.length() < 8 || userstr.length() > 12){
+                passFlag = false;
+                Toast userLimit = Toast.makeText(Create_Account.this, "Username needs to be at least 8 characters",Toast.LENGTH_SHORT);
+                userLimit.show();
+            }
             if(!passstr.equals(confrimpassstr)){
                 // error message
+                passFlag = false;
                 Toast pass = Toast.makeText(Create_Account.this, "Passwords do not match",Toast.LENGTH_SHORT);
                 pass.show();
             }
+
+            if(passstr.length() < 8 || passstr.length() > 12){
+                passFlag = false;
+                Toast passLimit = Toast.makeText(Create_Account.this, "Password needs to be at least 8 characters",Toast.LENGTH_SHORT);
+                passLimit.show();
+            }
+
             if(!emailstr.equals(confirmemstr)){
                 // error message
+                emailFlag = false;
                 Toast mail = Toast.makeText(Create_Account.this, "Email does not match", Toast.LENGTH_SHORT);
                 mail.show();
             }
-            else{
+            if((passFlag == true) && (emailFlag == true)){
                 //add to database
-                Toast message = Toast.makeText(Create_Account.this, "Reached add to database", Toast.LENGTH_SHORT);
+                Toast message = Toast.makeText(Create_Account.this, "Added account to database", Toast.LENGTH_SHORT);
                 message.show();
                 User user = new User();
                 user.setUsername(userstr);
