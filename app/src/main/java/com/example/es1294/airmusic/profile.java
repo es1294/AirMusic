c43eb95a572bcfdb588bc116342211efe934f9eb
annaspri <annaspri@buffalo.edu>
2018-11-13 01:28:14 -0500

    Established basic connection with realtime database. ( Closed task #88 )

--- app/src/main/java/com/example/es1294/airmusic/profile.java
+++ app/src/main/java/com/example/es1294/airmusic/profile.java
@@ -3,6 +3,7 @@ package com.example.es1294.airmusic;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -16,6 +17,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.HashMap;
 
 import static android.content.Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
@@ -24,7 +31,9 @@ public class profile extends AppCompatActivity {
 
     private Button profileEditButton;
     DatabaseHelper helper = new DatabaseHelper(this);
-
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("Profile");
+    TextView nameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,8 +86,8 @@ public class profile extends AppCompatActivity {
        profileView.setImageBitmap(bitmap);
 
        String fullName = user.getFullName();
-       TextView nameView = (TextView) findViewById(R.id.viewFullName);
-       nameView.setText(fullName);
+       nameView = (TextView) findViewById(R.id.viewFullName);
+ //      nameView.setText(fullName);
 
         String about = user.getAbout();
         TextView viewAbout = (TextView) findViewById(R.id.viewAboutContent);
@@ -104,6 +113,24 @@ public class profile extends AppCompatActivity {
         TextView genresView = (TextView) findViewById(R.id.viewFavoriteGenreContent);
         genresView.setText(formatGenresString);
 
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                nameView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void openProfileEditActivity(){
